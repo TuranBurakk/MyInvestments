66f786da36a4b02e080e133bdc412e5203502d74
burakTuran <buraktt00@gmail.com>
2023-09-01 17:50:30 +0300

    [MyInvestmentsApp][Created architectural]

---
+++ app/src/main/java/com/example/myinvestments/utils/Constans.kt
@@ -0,0 +1,6 @@
+package com.example.myinvestments.utils
+
+object Constans{
+    const val API_KEY = "apikey 5VEzUtIMJ3SB2cbAfckyMo:11AVuZ5PAaE5UC6whJXmM0"
+    const val BASE_URL = "https://api.collectapi.com"
+}
